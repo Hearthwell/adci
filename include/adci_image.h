f899bb6c9fb5ef31eff4722deee22e5f0e70f841
HearthWell <adam.el-houssami@polymtl.ca>
2024-07-03 22:40:00 -0400

    added basic support to get tensor from image

---
+++ include/adci_image.h
@@ -0,0 +1,8 @@
+#ifndef ADCI_IMAGE_H
+#define ADCI_IMAGE_H
+
+#include "adci_tensor.h"
+
+struct adci_tensor * adci_tensor_from_image(const char *path);
+
+#endif //ADCI_IMAGE_H
---
+++ src/adci_image.c
@@ -0,0 +1,17 @@
+#include "adci_image.h"
+#include "adci_tensor_op.h"
+
+#include "stb_image.h"
+
+struct adci_tensor * adci_tensor_from_image(const char *path){
+    /* BATCH VALUE SET TO 1 */
+    int shape[4] = {1, 0, 0, 0};
+    unsigned char *data = stbi_load(path, shape + 1, shape + 2, shape + 3, 0);
+    struct adci_tensor *tensor = adci_tensor_init(sizeof(shape) / sizeof(unsigned int), (unsigned int *)shape, ADCI_F32);
+    adci_tensor_alloc(tensor);
+    const unsigned int element_count = adci_tensor_element_count(tensor);
+    for(unsigned int i = 0; i < element_count; i++)
+        ((float *)tensor->data)[i] = (float)(data[i]);
+    ADCI_FREE(data);
+    return tensor;
+}
